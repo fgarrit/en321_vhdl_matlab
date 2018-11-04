4e36affda491ccafea44d1c0721d319eeabdf992
ritonlamenace <henri.roques@gmail.com>
2018-11-04 12:02:34 +0100

    P2S terminé, reste à tester

--- fichiers/vhdl/P2S.vhd
+++ fichiers/vhdl/P2S.vhd
@@ -13,35 +13,92 @@ port(
 end P2S;
 
 architecture Behavioral of P2S is
-signal cmp : integer := 0;
+signal cmp : integer range 0 to 9 := 0;
+signal cmpclk : integer range 0 to 9 := 0;
+signal start_cmpclk : std_logic;
+signal load_data : std_logic;
+signal cmp_load : integer range 0 to 6 := 0;
 signal stock : std_logic_vector(6 downto 0) := (others => '0'); 
 begin
 
-   process(clk, rst)
+   process(clk, reset)
    begin
-     if (reset = '1')   then
-         serial_data <= '0';
-         serial_data_valid <= '0';
-
+      if (reset = '1')   then
+         start_cmpclk <= '0';
       elsif rising_edge(clk) then
-
-            serial_data_valid <= '0'; -- 0 par défaut
-
-            if load = '1' then
-            cmp <= cmp + 1;
-            
-            if cmp = 7 then
-               cmp <= 0;
-               serial_data_valid <= '1'; -- 7ème bit reçu, les registres sont prêts et la valeur de sortie est juste
-
+            if load = '1' then 
+               start_cmpclk <= '1';
+            elsif cmp_load = 7 then
+               start_cmpclk <= '0'; 
             end if;
-
-            stock <= stock (2 downto 0) & serial_data; -- Registre à décalage
-
-         end if;
-      end if;
+      end if;      
    end process;
 
-   o_parrallel_data <= stock; -- On relie les registres à la sortie
+   process(clk, reset)
+   begin
+      if (reset = '1')   then
+         cmpclk <= 0;
+      elsif rising_edge(clk) then
+         if start_cmpclk = '1' then
+            if cmpclk > 9 then
+               cmpclk <= 0;
+            else 
+               cmpclk <= cmpclk + 1;
+            end if;
+         else
+            cmpclk <= 0;
+         end if;      
+      end if;   
+   end process;
 
+   process(clk, reset) --pause
+   begin
+      if cmpclk = 10 then
+         load_data <= '1';
+      else 
+         load_data <= '0';           
+      end if ;
+   end process;
+
+   process (reset, clk)
+   begin
+      if reset = '1' then
+        cmp_load <= 0;
+    elsif rising_edge (clk) then
+        if load = '1' then
+            cmp_load <= 0;
+        end if;
+        if load_data = '1' then
+            if cmp_load > 6 then
+                cmp_load <= 0;
+            else
+                cmp_load <= cmp_load +1;
+            end if;
+        end if;
+    end if;
+   end process;
+
+process(reset, clk)
+begin
+    if reset = '1' then
+        stock <= "0000000";
+    elsif rising_edge(clk) then
+        if load='1' then
+            stock <= par_data;
+        elsif load_data = '1' then
+            stock <= stock( 5 downto 0) & stock(6);
+        end if;
+    end if;
+end process;  
+
+process(load_data, reset)
+begin
+    if reset = '1' then
+        serial_data <= '0';
+    elsif load_data ='1' then
+        serial_data <= data_temp(6);
+    end if;
+end process;
+
+serial_data_valid <= load_data;
 end Behavioral;
