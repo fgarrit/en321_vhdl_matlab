b92b52d0818399afc93073cf09aa955e07e2420e
ritonlamenace <henri.roques@gmail.com>
2018-11-05 10:45:28 +0100

    P2S refait, a tester

--- fichiers/vhdl/P2S.vhd
+++ fichiers/vhdl/P2S.vhd
@@ -1,104 +1,82 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity P2S is
-generic (width: integer := 7);
-port( 
-      clk                  : in std_logic;
-      reset                : in std_logic;
-      load                 : in std_logic;
-      par_data             : in std_logic_vector(width-1 downto 0);
-      serial_data          : out std_logic;
-      serial_data_valid    : out std_logic);
-end P2S;
+entity p2s is
+    Generic(width             : integer := 7);
+    Port ( clk                : in STD_LOGIC;
+           reset              : in STD_LOGIC;
+           load               : in STD_LOGIC;
+           par_data           : in STD_LOGIC_VECTOR (width-1 downto 0);
+           serial_data        : out STD_LOGIC;
+           serial_data_valid  : out STD_LOGIC);
+end p2s;
 
-architecture Behavioral of P2S is
-signal cmp : integer range 0 to 9 := 0;
-signal cmpclk : integer range 0 to 9 := 0;
-signal start_cmpclk : std_logic;
-signal load_data : std_logic;
-signal cmp_load : integer range 0 to 6 := 0;
-signal stock : std_logic_vector(6 downto 0) := (others => '0'); 
+architecture Behavioral of p2s is
+
+signal Cnt                 : integer range 0 to 9 := 0 ;
+signal cnt_horloge         : unsigned (3 downto 0) ;
+signal start_cnt_h         : std_logic ;
+signal load_data           : std_logic;
+signal cnt_load            : integer range 0 to 6 := 0 ;
+signal data_temp           : std_logic_vector (6 downto 0) := (others => '0');
+signal serial_data_t       : std_logic;
+signal serial_data_valid_t : std_logic;
+
 begin
 
-   process(clk, reset)
-   begin
-      if (reset = '1')   then
-         start_cmpclk <= '0';
-      elsif rising_edge(clk) then
-            if load = '1' then 
-               start_cmpclk <= '1';
-            elsif cmp_load = 7 then
-               start_cmpclk <= '0'; 
-            end if;
-      end if;      
-   end process;
-
-   process(clk, reset)
-   begin
-      if (reset = '1')   then
-         cmpclk <= 0;
-      elsif rising_edge(clk) then
-         if start_cmpclk = '1' then
-            if cmpclk > 9 then
-               cmpclk <= 0;
-            else 
-               cmpclk <= cmpclk + 1;
-            end if;
-         else
-            cmpclk <= 0;
-         end if;      
-      end if;   
-   end process;
-
-   process(clk, reset) --pause
-   begin
-      if cmpclk = 10 then
-         load_data <= '1';
-      else 
-         load_data <= '0';           
-      end if ;
-   end process;
-
-   process (reset, clk)
-   begin
-      if reset = '1' then
-        cmp_load <= 0;
-    elsif rising_edge (clk) then
-        if load = '1' then
-            cmp_load <= 0;
-        end if;
-        if load_data = '1' then
-            if cmp_load > 6 then
-                cmp_load <= 0;
-            else
-                cmp_load <= cmp_load +1;
-            end if;
-        end if;
-    end if;
-   end process;
-
-process(reset, clk)
+Process (reset, clk)
 begin
     if reset = '1' then
-        stock <= "0000000";
-    elsif rising_edge(clk) then
-        if load='1' then
-            stock <= par_data;
-        elsif load_data = '1' then
-            stock <= stock( 5 downto 0) & stock(6);
+        data_temp <= (others => '0');
+    elsif clk'event and clk = '1' then
+        if load = '1' then 
+            data_temp <= par_data;
+        elsif cnt_horloge < 6 then
+            data_temp <= data_temp(5 downto 0) & data_temp(6);
+        else 
+            data_temp <= data_temp;
         end if;
     end if;
-end process;  
-
-process(load_data, reset)
+end process;
+    
+Process (reset, clk)
 begin
     if reset = '1' then
-        serial_data <= '0';
-    elsif load_data ='1' then
-        serial_data <= stock(6);
+        cnt_horloge <= (others => '0');
+    elsif clk'event and clk = '1' then
+        if load = '1' then 
+            cnt_horloge <= (others => '0');
+        elsif start_cnt_h = '1' then
+            cnt_horloge <= cnt_horloge + 1 ; 
+        end if;
+    end if;
+end process;
+    
+Process (reset, clk)
+begin
+    if reset = '1' then
+        start_cnt_h <= '0';
+    elsif clk'event and clk = '1' then
+        if load = '1' then 
+            start_cnt_h <= '1';
+        elsif cnt_horloge = 6 then
+            start_cnt_h <= '0' ; 
+        end if;
     end if;
 end process;
 
-serial_data_valid <= load_data;
+Process (reset, clk)
+begin
+    if reset = '1' then
+        serial_data <= '0';
+        serial_data_valid <= '0';
+    elsif clk'event and clk = '1' then
+        if start_cnt_h = '1' then 
+            serial_data <= data_temp(6);
+            serial_data_valid <= '1';
+        else
+            serial_data_valid <= '0';
+        end if;
+    end if;
+end process;
 end Behavioral;
